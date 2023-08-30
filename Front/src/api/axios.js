be97df6660de16a5ace2dacc5d4b49e779e5bca6
claudio <contacto.claudio@gmail.com>
2023-08-30 16:23:19 -0400

    se quita withCredential en axios.js

--- Front/src/api/axios.js
+++ Front/src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
     /* baseURL: 'http://localhost:3090/api', */
-    baseURL: 'https://magyprintbd.onrender.com/api',
-    withCredentials: true
+    baseURL: 'https://magyprintbd.onrender.com/api'
+    /* withCredentials: true */
 })
 
 
