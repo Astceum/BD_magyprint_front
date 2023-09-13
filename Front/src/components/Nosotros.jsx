a16177d05416a311795393913593d8250958bcad
Mauricio <m.millavil.escobar@gmail.com>
2023-09-13 17:14:13 -0300

    modificacion card empresa

--- Front/src/components/Nosotros.jsx
+++ Front/src/components/Nosotros.jsx
@@ -10,7 +10,7 @@ function Nosotros() {
         </div>
         <div className="col-md-8 columna">
           <h2 className="titulo">Una empresa basada en la familia</h2>
-          <p className="descripcion">La historia de Margarita, conocida cariñosamente como Maggy, es la de una madre, cuya vida cambió cuando su hijo fue diagnosticado con Autismo. Con el deseo de dedicar más tiempo a su hijo y generar ingresos, crea su negocio llamado "Magyprint", una tienda de productos personalizados y originales, permitiendo a los clientes enviar sus propios diseños. Con el apoyo de su esposo y clientes satisfechos, el negocio crece. Magyprint se convirtió en una prueba del poder del amor la constancia y la creatividad para superar los obstáculos, inspirando a otros la originalidad en cada regalo.</p>
+          <p className="descripcion">La historia de Margarita, conocida cariñosamente como Maggy, es la de una madre cuya vida cambió cuando su hijo fue diagnosticado con Autismo. Con el deseo de dedicar más tiempo a su hijo y generar ingresos, crea su negocio llamado "Magyprint", una tienda de productos personalizados y originales, permitiendo a los clientes enviar sus propios diseños. Con el apoyo de su esposo y clientes satisfechos, el negocio crece. Magyprint se convirtió en una prueba del poder del amor la constancia y la creatividad para superar los obstáculos, inspirando a otros la originalidad en cada regalo.</p>
         </div>
       </div>
     </div>
